f300fb3752aa2f1fa64cd022b07f1618f49fcb75
Jason Nelson <Jasonnelson11@gmail.com>
2019-02-12 00:36:16 -0500

    Changed the design and added a photo

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+var nodemailer = require('nodemailer');
+
+$("#contact-submit").on("click", function(){
+event.preventDefault();
+//Variables for the content of the gmail
+var Name = $("#Name").value();
+var Email = $("#Email").value();
+var Body = $("#Body").value();
+
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main(){
+
+  // create reusable transporter object using the default SMTP transport
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+           user: 'jasonfswd@gmail.com',
+           pass: 'Nelson2009'
+       }
+   });
+
+  // setup email data with unicode symbols
+  const mailOptions = {
+    from: Name,// sender address
+    to: Email, // list of receivers
+    subject: 'Inquiry For Website', // Subject line
+    html: Body // plain text body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info);
+ });
+}
+
+main().catch(console.error);
+})
