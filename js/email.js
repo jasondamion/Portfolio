87e9cb556689de13afe65d0754812a7e9646f283
Jason Nelson <jasonnelson11@gmail.com>
2022-11-30 17:49:56 -0500

    Updating with latest code

--- js/email.js
+++ js/email.js
@@ -1,38 +1,57 @@
-$("#subEmail").on("click", function(){
-axios.post('https://email-servo.herokuapp.com/portfolio', {
-    name: $("#nameForEmail").val(),
-    email: $("#emailForEmail").val(),
-    message: $("#messForEmail").val()
-  })
-  .then(function (response) {
-    setTimeout(function(){
-      $("#thanks-modal").css("opacity","1")
-      $('#thanks-modal').show('fast')
-    },1000)
-  })
-  .catch(function (error) {
-    setTimeout(function(){
-      $("#error-modal").css("opacity","1")
-      $('#error-modal').show('fast')
-    },1000)
-  });
-})
+$("#subEmail").on("click", function () {
+  let name = $("#nameForEmail");
+  let email = $("#emailForEmail");
+  let message = $("#messForEmail");
 
-$("#subEmailMod").on("click", function(){
+  if (name.valid() && email.valid() && message.valid()) {
     axios.post('https://email-servo.herokuapp.com/portfolio', {
-        name: $("#nameForEmailMod").val(),
-        email: $("#emailForEmailMod").val(),
-        message: $("#messForEmailMod").val()
-      })
+      name: name.val(),
+      email: email.val(),
+      message: message.val()
+    })
       .then(function (response) {
-        setTimeout(function(){
-          $("#thanks-modal").css("opacity","1")
+        setTimeout(function () {
+          $("#thanks-modal").css("opacity", "1")
           $('#thanks-modal').show('fast')
-        },1000)
+          name.val("")
+          email.val("")
+          message.val("")
+        }, 1000)
       })
       .catch(function (error) {
-        setTimeout(function(){
-          $("#error-modal").css("opacity","1")
+        setTimeout(function () {
+          $("#error-modal").css("opacity", "1")
           $('#error-modal').show('fast')
-        },1000)      });
+        }, 1000)
+      });
+  }
+})
+
+$("#subEmailMod").on("click", function () {
+  let name = $("#nameForEmailMod");
+  let email = $("#emailForEmailMod");
+  let message = $("#messForEmailMod");
+
+  if (name.valid() && email.valid() && message.valid()) {
+    axios.post('https://email-servo.herokuapp.com/portfolio', {
+      name: name.val(),
+      email: email.val(),
+      message: message.val()
     })
+      .then(function (response) {
+        setTimeout(function () {
+          $("#thanks-modal").css("opacity", "1")
+          $('#thanks-modal').show('fast')
+          name.val("")
+          email.val("")
+          message.val("")
+        }, 1000)
+      })
+      .catch(function (error) {
+        setTimeout(function () {
+          $("#error-modal").css("opacity", "1")
+          $('#error-modal').show('fast')
+        }, 1000)
+      });
+  }
+})
